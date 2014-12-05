38d9223d94823617acd6b8def17d8ad15f6a31dd
tzw-madzia <madzia.nowak.pl@gmail.com>
2014-12-05 06:55:34 +0100

    add some test configuration

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -2,15 +2,17 @@ ENV["RAILS_ENV"] ||= 'test'
 require 'spec_helper'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
-require 'rspec/autorun'
 require 'capybara/rails'
 require 'capybara/rspec'
+require 'support/database_cleaner'
 
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
+  config.infer_base_class_for_anonymous_controllers = false
+  config.order = "random"
   config.infer_spec_type_from_file_location!
   config.include Capybara::DSL
   Capybara.server_host = 'localhost'
